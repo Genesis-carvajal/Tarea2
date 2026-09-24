c0eb58bc9d5d587c69036e745b52e77150e02706
Genesis-carvajal <genesis.carvajalsanchez@ucr.ac.cr>
2026-09-23 22:43:57 -0600

    Clase Menu empezada

--- src/main/java/main/Menu.java
+++ src/main/java/main/Menu.java
@@ -4,10 +4,46 @@
  */
 package main;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author carva
  */
 public class Menu {
+    private int opcion;
     
-}
+    public void menuPrincipal() {
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("""
+                                                                  BIENVENIDO, INGRESE UN NÚMERO.
+                                                                  1. Registrar producto. 
+                                                                  2. Mostrar productos. 
+                                                                  3. Buscar producto por código. 
+                                                                  4. Vender unidades. 
+                                                                  5. Reabastecer producto. 
+                                                                  6. Calcular valor total del inventario. 
+                                                                  7. Salir.
+                                                                  """));
+            switch(opcion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                default:
+            }
+            
+        } while (opcion != 7);
+    }//Cierre método menuPrincipal.
+    
+}//Fin clase menú.
--- src/main/java/main/Principal.java
+++ src/main/java/main/Principal.java
@@ -11,6 +11,6 @@ package main;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Menu m = new Menu();
     }
 }
