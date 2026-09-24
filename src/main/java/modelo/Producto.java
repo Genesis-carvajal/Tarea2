5885149cce55cbdcd37f4ced84872164f3e64d99
Genesis-carvajal <genesis.carvajalsanchez@ucr.ac.cr>
2026-09-23 21:50:43 -0600

    Clase Producto

--- src/main/java/modelo/Producto.java
+++ src/main/java/modelo/Producto.java
@@ -9,5 +9,49 @@ package modelo;
  * @author carva
  */
 public class Producto {
+
+    private int codigo;
+    private String nombre;
+    private double precio;
+    private int cantidadDisponible;
+
+    //CONSTRUCTOR VACÍO.
+    public Producto() {
+    }
+
+    //CONSTRUCTOR CON PARÁMETROS.
+    public Producto(int codigo, String nombre, double precio, int cantidadDisponible) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidadDisponible = cantidadDisponible;
+    }
     
+    //SET.
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+    
+    //GET.
+    public int getCodigo() {
+        return codigo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
 }
